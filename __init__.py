b0a20cf0e51939bc5dd5e08632504f77f0f5bd67
Hichigo <hichigo91@gmail.com>
2020-10-31 21:56:18 +0300

    work on 2.9 with thread

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ bl_info = {
     "name": "Nexus Arduino Capture",
     "author": "Nexus Studio",
     "version": (0, 0, 1),
-    "blender": (2, 79, 0),
+    "blender": (2, 90, 1),
     "location": "View 3D > Tool Shelf",
     "description": "Tools",
     "warning": "",
@@ -18,23 +18,60 @@ import serial
 import threading
 
 import bpy
-from bpy.props import *
+from bpy.props import EnumProperty, BoolProperty
 import bpy.utils.previews
 from math import radians
 
-arduino_data = serial.Serial()
+arduino = serial.Serial()
 
-def frameChange(passedScene):
-    data = arduino_data.readline().strip()
+class RotateCubeThread(threading.Thread):
+    def __init__(self, threadID, name):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
 
-    # rot_angle = float(data.decode())
-    data = str(data.decode())
-    data_YPR = [float(val) for val in data.split(',')]
-    # print(data_split, type(data_split))
+    def get_serial_data(self):
+        data = arduino.readline()[:-2] #the last bit gets rid of the new-line chars
+        if data == b'0' or data == b'':
+            data = b"0|0|0|0|0|0"
+        try:
+            data = data.decode("utf-8").split("|")
 
-    bpy.context.active_object.rotation_euler = (radians(data_YPR[0]), radians(data_YPR[1]), radians(data_YPR[2]))
-    #bpy.context.active_object.location.x = rot_angle
+        except:
+            print("Value error!!!")
+        if data:
+            result = []
+            for item in data:
+                result.append(int(item))
 
+        return result
+
+    def run(self):
+        FPS = 1
+        # arduino_prop = bpy.context.scene.arduino_prop.captured
+
+        tick = time.time()
+        loop = 0
+        x = 0
+        cube = bpy.data.objects["Cube"]
+
+        while bpy.context.scene.arduino_prop.captured is True:
+            # ax, ay, az, gx, gy, gz
+            transform = self.get_serial_data()
+            # cube.rotation_mode = 'XYZ'
+            # cube.rotation_euler = (transform[3]/2000, transform[4]/2000, transform[5] /2000)
+            print(transform)
+
+            if not transform:
+                print("buffer")
+            else:
+                #knob.rotation_euler = ( rotation_degrees[0] * 0.00875, rotation_degrees[1] * 0.00875, rotation_degrees[2] * 0.00875)
+                # cube.location.y += 0
+                pass
+            tick = time.time()
+
+            loop += 1
+            x += 1
 
 def enum_COMports_list(self, context):
     ports = serial_ports()
@@ -79,13 +116,15 @@ class Start_Capture(bpy.types.Operator):
 
         arduino_prop.captured = True
 
-        arduino_data.baudrate = 115200 #9600
-        arduino_data.port = arduino_prop.COM_ports
+        arduino.baudrate = 9600 #115200
+        arduino.port = arduino_prop.COM_ports
 
-        arduino_data.open()
+        arduino.open()
 
-        print(arduino_data)
-        bpy.app.handlers.scene_update_pre.append(frameChange)
+        print(arduino)
+        #Make thread
+        thread = RotateCubeThread(1, "thread")
+        thread.start()
 
         return {'FINISHED'}
 
@@ -98,17 +137,19 @@ class Stop_Capture(bpy.types.Operator):
 
         arduino_prop = context.scene.arduino_prop
         arduino_prop.captured = False
-        bpy.app.handlers.scene_update_pre.remove(frameChange)
-        arduino_data.close()
+        arduino.close()
 
         return {'FINISHED'}
 
 class CapturePanel(bpy.types.Panel):
+    """ Arduino capture panel """
 
     bl_label = "Arduino Capture"
+    bl_idname = "VIEW3D_PT_CapturePanel"
     bl_space_type = 'VIEW_3D'
-    bl_region_type = 'TOOLS'
-    bl_category = "Arduino Capture"
+    bl_region_type = 'UI'
+    bl_category = "Nexus"
+    bl_options = {"DEFAULT_CLOSED"}
 
     @classmethod
     def poll(cls, context):
@@ -130,36 +171,41 @@ class CapturePanel(bpy.types.Panel):
         row.operator("capture.start", icon="PLAY", text="Start capture")
         row = col.row(align=True)
         row.enabled = arduino_prop.captured
-        row.operator("capture.stop", icon="MESH_PLANE", text="Stop capture")
+        row.operator("capture.stop", icon="CANCEL", text="Stop capture")
 
 
 
-class ArduinoCapture_Scene_prop(bpy.types.PropertyGroup):
-    COM_ports = EnumProperty(
+class ArduinoCaptureProp(bpy.types.PropertyGroup):
+    COM_ports: EnumProperty(
         items=enum_COMports_list
     )
-
-    captured = BoolProperty(
+    captured: BoolProperty(
         name="Captured",
         description="capture start / stop",
         default=False
     )
 
+classes = (
+    ArduinoCaptureProp,
+    Start_Capture,
+    Stop_Capture,
+    CapturePanel
+)
+
+
 def register():
-    bpy.utils.register_class(Start_Capture)
-    bpy.utils.register_class(Stop_Capture)
+    from bpy.utils import register_class
+    for cls in classes:
+        register_class(cls)
 
-    bpy.utils.register_module(__name__)
-
-    bpy.types.Scene.arduino_prop = bpy.props.PointerProperty(type=ArduinoCapture_Scene_prop)
+    bpy.types.Scene.arduino_prop = bpy.props.PointerProperty(type=ArduinoCaptureProp)
 
 def unregister():
-    bpy.utils.unregister_class(Start_Capture)
-    bpy.utils.unregister_class(Stop_Capture)
+    from bpy.utils import unregister_class
+    for cls in reversed(classes):
+        unregister_class(cls)
 
     del bpy.types.Scene.arduino_prop
-
-    bpy.utils.unregister_module(__name__)
 
 if __name__ == '__main__':
     register()
